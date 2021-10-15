e01caf0eb6c3980a979fb35950b2caab8ba3fc2c
Nripoll02 <nahuelripoll33@gmail.com>
2021-10-15 16:56:00 -0300

    Clase 14 Lab

---
+++ DesafioInterfaces/Impresora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioInterfaces
+{
+    class Impresora
+    {
+        List<Imprimible> ColaDeImpresiones = new List<Imprimible>();
+
+
+
+        public void ImprimirTodo()
+        {
+            foreach (Imprimible c in ColaDeImpresiones)
+                c.Imprimir();
+        }
+        public void AgregarImprimible()
+        {
+            Documento documento = new Documento();
+            ColaDeImpresiones.Add(documento);
+
+            Foto foto = new Foto();
+            ColaDeImpresiones.Add(foto);
+
+            Contrato contrato = new Contrato();
+            ColaDeImpresiones.Add(contrato);
+        }
+
+    }
+}
---
+++ DesafioInterfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foto foto = new Foto();
+            Documento documento = new Documento();
+            Contrato contrato = new Contrato();
+            Impresora impresora = new Impresora();
+            impresora.AgregarImprimible();
+            impresora.ImprimirTodo();
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
