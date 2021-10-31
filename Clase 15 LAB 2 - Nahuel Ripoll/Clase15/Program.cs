a0552729b52cf4610ecc36f4a8fac0e8148606c3
Nahuel Ripoll <nahuelripoll33@gmail.com>
2021-10-31 18:58:21 -0300

    Resolucion de desafios

---
+++ Clase 15 LAB 2 - Nahuel Ripoll/Clase15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Clase15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lavadora myLavadora = new Lavadora();
+            Television myTelevision = new Television();
+            int opcion;
+            Console.WriteLine("Por favor, elija un electrodomestico");
+            Console.WriteLine("Televisior(1)");
+            Console.WriteLine("Lavadora(2)");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    myTelevision.PrecioFinal();
+                    break;
+                case 2:
+                    myLavadora.precioFinal();
+                    break;
+                default:
+                    break;
+            }
+            
+
+
+        }
+    }
+}
